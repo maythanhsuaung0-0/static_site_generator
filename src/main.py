573b06380c029826da8a8e83a68e7f2315e95123
maythanhsuaung0-0 <maythanhsuaung1111@gmail.com>
2025-09-30 11:04:11 -0400

    added tests, textNode, and Text type classes

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    textNode = TextNode("hello", TextType.BOLD, "")
+    print(textNode)
+main()
---
+++ test.sh
@@ -0,0 +1,2 @@
+# run all the tests under src folder
+python3 -m unittest discover -s src
